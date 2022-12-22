73a1974c4908a5b9b17ef35b11e750e5bbc52430
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2022-12-22 16:05:08 +0600

    Single post

--- components/SinglePostCard.js
+++ components/SinglePostCard.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import Link from 'next/link';
+import { Card } from 'antd';
+import { Typography } from 'antd';
+const { Meta } = Card;
 
 const SinglePostCard = ({data}) => {
+
   return (
-    <div>{data?.title}</div>
+    <Card
+    // hoverable
+    style={{
+      width: '50vw',
+    }}
+    // cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+      <Link
+        href="/post/[pid]"
+        as={`/product/${data?.id}`}
+      >
+        <Meta title={data?.title} />
+      </Link>
+      <Typography.Title level={5} style={{ margin: 0 }}>
+        {data.author}
+      </Typography.Title>
+    
+  </Card>
+
   )
 }
 
